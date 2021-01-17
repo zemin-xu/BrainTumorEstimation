70e7969dbf5e318ae118160166c82ff37044b38c
zemin xu <zemin.xu@yahoo.com>
2021-01-17 19:35:48 +0100

    first commit

---
+++ Assets/BrainTumor/BrainData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BrainData
+{
+    public string dataName;
+    public Vector3 boxCenter;
+    public Vector3 boxDimension;
+}
