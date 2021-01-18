7ab94301ac9f102f92ce2c1fe792422871eca00c
zemin xu <zemin.xu@yahoo.com>
2021-01-18 15:04:01 +0100

    new update

--- Assets/BrainTumor/Options.cs
+++ Assets/BrainTumor/Options.cs
@@ -10,8 +10,50 @@ using System.Linq;
 public class Options : MonoBehaviour
 {
     public string dataIndex;
+    public TextAsset jsonFile;
 
-    private void Import(string dir)
+    private VolumeRenderedObject brain;
+    private VolumeRenderedObject brainSeg;
+
+    private GameObject boundingBox;
+    private Renderer rend;
+    private Bounds bounds;
+
+    public void OnBoundingBoxButtonClicked()
+    {
+        boundingBox = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        BrainData loadedData = ReadData();
+        boundingBox.transform.position = loadedData.boxCenter;
+        boundingBox.transform.localScale = loadedData.boxDimension;
+
+        // output brain size
+        //rend = brain.transform.GetComponent<Renderer>();
+        //bounds = brain.transform.GetComponent<MeshFilter>().mesh.bounds;
+
+        //Vector3 center = rend.bounds.center;
+        //float radius = rend.bounds.extents.magnitude;
+
+        //float x = rend.bounds.extents.x;
+        //float y = rend.bounds.extents.y;
+        //float z = rend.bounds.extents.z;
+
+        //Debug.Log("radius: " + radius);
+        //Debug.Log("x: " + x);
+        //Debug.Log("y: " + y);
+        //Debug.Log("z: " + z);
+
+        //Vector3 c = bounds.center;
+        //float r = bounds.extents.magnitude;
+        //Debug.Log("r: " + r);
+    }
+
+    public BrainData ReadData()
+    {
+        return JsonUtility.FromJson<BrainData>(jsonFile.text);
+    }
+
+    private VolumeRenderedObject Import(string dir)
     {
         if (Directory.Exists(dir))
         {
@@ -27,22 +69,26 @@ public class Options : MonoBehaviour
                 VolumeDataset dataset = importer.Import();
                 if (dataset != null)
                 {
-                    VolumeRenderedObject volumeObj = VolumeObjectFactory.CreateObject(dataset);
-                    volumeObj.transform.Rotate(new Vector3(180, 0, 0));
+                    VolumeRenderedObject vo = VolumeObjectFactory.CreateObject(dataset);
+                    vo.transform.Rotate(new Vector3(180, 0, 0));
+                    return vo;
                 }
             }
             else
+            {
                 Debug.LogError("Could not find any DICOM files to import.");
+            }
         }
+        return null;
     }
 
     public void OnImportButtonClicked()
     {
-        Import(dataIndex + "_flair");
+        brain = Import(dataIndex + "_flair");
     }
 
     public void OnSegmentationButtonClicked()
     {
-        Import(dataIndex + "_seg");
+        brainSeg = Import(dataIndex + "_seg");
     }
 }
